ff78960d4c631c028335ad6f39aefc09e1096f87
daniel Mahlaka <dmahlaka22@gmail.com>
2023-07-18 03:44:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - function prints text as output
+  *
+  * Return: return 0
+*/
+int main(void)
+{
+	char School[10] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(School[i]);
+	}
+	_putchar(10);
+	return (0);
+}
